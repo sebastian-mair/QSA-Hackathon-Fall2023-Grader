93057d5b96b48165bea9a7e5084c7c604cea6572
sebastian-mair <45604440+sebastian-mair@users.noreply.github.com>
2023-07-06 12:03:41 +0200

    add another test

--- test_add.py
+++ test_add.py
@@ -1,5 +1,10 @@
 import importlib  
 hackathon = importlib.import_module("QSA-Hackathon-Fall2023.adder")
 
+
 def test_add():
   assert hackathon.add(3, 4) == 7
+
+
+def silly_test_add():
+  assert hackathon.add(3, -4) == 7
